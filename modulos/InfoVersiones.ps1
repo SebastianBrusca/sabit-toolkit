b8162d506b641ae518a98cc2115f2c3b9f7d938f
sebastian ariel brusca <37081031+SebastianBrusca@users.noreply.github.com>
2026-04-28 20:08:37 -0300

    Update InfoVersiones.ps1

--- modulos/InfoVersiones.ps1
+++ modulos/InfoVersiones.ps1
@@ -29,3 +29,5 @@ function InfoVersiones {
 
     Write-Host "================================" -ForegroundColor Cyan
 }
+# ================= EJECUTAR AUTOMÁTICAMENTE =================
+InfoVersiones
