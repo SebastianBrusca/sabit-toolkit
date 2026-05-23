1cda3d30ccb59b330e0cc2fd8f09ba2b82d9fd9d
sebastian ariel brusca <37081031+SebastianBrusca@users.noreply.github.com>
2026-05-23 14:23:02 -0300

    Update sabit.ps1

--- sabit.ps1
+++ sabit.ps1
@@ -63,7 +63,7 @@ function Mostrar-Banner {
     Write-Host ""
     Write-Host "              SABIT - SOPORTE TECNICO " -ForegroundColor Green
     Write-Host "====================================================" -ForegroundColor Cyan
-    Write-Host "             Version 0.70             Hora: " -NoNewline -ForegroundColor Green
+    Write-Host "             Version 0.71             Hora: " -NoNewline -ForegroundColor Green
     Write-Host $hora -ForegroundColor Green
     Write-Host "====================================================" -ForegroundColor Cyan
     Write-Host ""
@@ -76,8 +76,8 @@ function Leer-Opcion-Con-Hora {
     $inputX = [Console]::CursorLeft
     $inputY = [Console]::CursorTop
 
-    $horaX = 45
-    $horaY = 6
+    $horaX = 42
+    $horaY = 10
 
     $texto = ""
 
