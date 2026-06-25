432eee3fd2f3bff02663233175db34b365a6be0c
sebastian ariel brusca <37081031+SebastianBrusca@users.noreply.github.com>
2026-06-25 00:48:29 -0300

    Update banner version and maintain structure

--- sabit.ps1
+++ sabit.ps1
@@ -63,7 +63,7 @@ function Mostrar-Banner {
     Write-Host ""
     Write-Host "              SABIT - SOPORTE TECNICO " -ForegroundColor Green
     Write-Host "====================================================" -ForegroundColor Cyan
-    Write-Host "                   Version 1.1.87           " -ForegroundColor Green
+    Write-Host "                   Version 1.1.88           " -ForegroundColor Green
     Write-Host "====================================================" -ForegroundColor Cyan
     Write-Host ""
 }
